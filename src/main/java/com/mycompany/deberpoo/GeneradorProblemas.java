5812c3d56a9ebab840dc7978e19b579a64d8905d
Jessica Herrera <jherrera4055@utm.edu.ec>
2023-10-29 16:34:08 -0500

    Commit 2 GeneradorProblemas

--- src/main/java/com/mycompany/deberpoo/GeneradorProblemas.java
+++ src/main/java/com/mycompany/deberpoo/GeneradorProblemas.java
@@ -1,14 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.mycompany.deberpoo;
+import java.security.SecureRandom;
 
-/**
- *
- * @author LENOVO
- */
 public class GeneradorProblemas {
-    
+    private final int nivelDificultad;
+    private final int tipoProblema;
+    private final SecureRandom random;
+
+    public GeneradorProblemas(int nivelDificultad, int tipoProblema) {
+        this.nivelDificultad = nivelDificultad;
+        this.tipoProblema = tipoProblema;
+        this.random = new SecureRandom();
+    }
+
+    public int[] generarOperandos() {
+        int limite = (int) Math.pow(10, nivelDificultad) - 1;
+        int operando1 = random.nextInt(limite) + 1;
+        int operando2 = random.nextInt(limite) + 1;
+        return new int[]{operando1, operando2};
+    }
+
+    public String generarPregunta(int operando1, int operando2) {
+        switch (tipoProblema) {
+            case 1: return "¿Cuánto es " + operando1 + " + " + operando2 + "?";
+            case 2: return "¿Cuánto es " + operando1 + " - " + operando2 + "?";
+            case 3: return "¿Cuánto es " + operando1 + " x " + operando2 + "?";
+            case 4: return "¿Cuánto es " + operando1 + " / " + operando2 + "?";
+            default: return "Operación no reconocida";
+        }
+    }
+
+    public int calcularRespuesta(int operando1, int operando2) {
+        switch (tipoProblema) {
+            case 1: return operando1 + operando2;
+            case 2: return operando1 - operando2;
+            case 3: return operando1 * operando2;
+            case 4: return operando1 / operando2;
+            default: return 0;
+        }
+    }
 }
