52252237eebf9d3baa55f35103aea79dd9365d3d
Jessica Herrera <jherrera4055@utm.edu.ec>
2023-10-29 17:42:57 -0500

    Commit 4 RastreadorRendimiento completar

--- src/main/java/com/mycompany/deberpoo/RastreadorRendimiento.java
+++ src/main/java/com/mycompany/deberpoo/RastreadorRendimiento.java
@@ -7,5 +7,27 @@ public class RastreadorRendimiento {
         this.intentos = 0;
     }
 
+    public void registrarAcierto() {
+        aciertos++;
+        intentos++;
+    }
+
+    public void registrarFallo() {
+        intentos++;
+    }
+
+    public double calcularPorcentajeAciertos() {
+        return ((double) aciertos / (double) intentos) * 100.0;
+    }
+
+    public String generarMensajeFinal() {
+        double porcentaje = calcularPorcentajeAciertos();
+        if (porcentaje < 75.0) {
+            return "Por favor pide ayuda adicional a tu instructor.";
+        } else {
+            return "Felicidades, estás listo para pasar al siguiente nivel!";
+        }
+    }
 }
+
 
